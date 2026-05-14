3770038443963de4c240d557284a35d8e78da52e
Taufiq <taufiq.ismail@datakraf.com>
2026-05-15 01:07:07 +0800

    fix key length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\Models\ActivityLog;
 
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
+
         // Auto-save all pending activity logs at the end of each request
         app()->terminating(function () {
             ActivityLog::savePendingLogs();
